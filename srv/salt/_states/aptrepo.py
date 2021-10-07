3bf23d1d00636c125fd3d733c96d2fc91e09b972
Jonas Maurus <jonas-github@maurus.net>
2021-10-07 21:43:53 +0200

    make sure that aptrepo runs an apt-get update

--- srv/salt/_states/aptrepo.py
+++ srv/salt/_states/aptrepo.py
@@ -82,46 +82,46 @@ def managed(name, listfile_name, signing_key_url, signed_by, dearmor=True, arch=
     if signing_key_url.startswith("salt"):
         skip_verify = False
 
-    try:
-        log.debug("creating .list file %s", listfile_fn)
-        listfile_ret = __states__['file.managed'](name=listfile_fn,
-                                                  user="root",
-                                                  group="root",
-                                                  dir_mode="0755",
-                                                  contents=repo_str,
-                                                  makedirs=True,
-                                                  require=require)
-        log.debug(listfile_ret)
-        _propagate_changes(ret, listfile_ret)
+    log.debug("creating .list file %s", listfile_fn)
+    listfile_ret = __states__['file.managed'](name=listfile_fn,
+                                              user="root",
+                                              group="root",
+                                              dir_mode="0755",
+                                              contents=repo_str,
+                                              makedirs=True,
+                                              require=require)
+    log.debug(listfile_ret)
+    _propagate_changes(ret, listfile_ret)
 
-        if dearmor:
-            if not __salt__["file.file_exists"](signed_by):
-                tmp = salt.utils.files.mkstemp()
-                log.debug("downloading signed-by keyfile to %s from %s", tmp, signing_key_url)
-                keyfile_ret = __states__['file.managed'](name=tmp,
-                                                         source=signing_key_url,
-                                                         require=require,
-                                                         skip_verify=skip_verify)
-                log.debug(keyfile_ret)
-                _propagate_changes(ret, keyfile_ret)
+    if dearmor:
+        if not __salt__["file.file_exists"](signed_by):
+            tmp = salt.utils.files.mkstemp()
+            log.debug("downloading signed-by keyfile to %s from %s", tmp, signing_key_url)
+            keyfile_ret = __states__['file.managed'](name=tmp,
+                                                     source=signing_key_url,
+                                                     require=require,
+                                                     skip_verify=skip_verify)
+            log.debug(keyfile_ret)
+            _propagate_changes(ret, keyfile_ret)
 
-                log.debug("dearmoring %s to %s", tmp, signed_by)
-                req_cmd = require.append({"file": tmp}) if require else [{"file": tmp}]
-                dearmor_ret = __states__['cmd.run'](name="/usr/bin/gpg --dearmor -o '%s' '%s'" % (signed_by, tmp),
-                                                    creates=signed_by,
-                                                    require=req_cmd)
-                log.debug(dearmor_ret)
-                _propagate_changes(ret, dearmor_ret)
-                salt.utils.files.remove(tmp)
-        else:
-            if not __salt__["file.file_exists"](signed_by):
-                log.debug("storing dearmored key %s in %s", signing_key_url, signed_by)
-                keyfile_ret = __states__['file.managed'](name=signed_by,
-                                                         url=signing_key_url,
-                                                         skip_verify=skip_verify)
-                log.debug(keyfile_ret)
-                _propagate_changes(ret, keyfile_ret)
-    except ExecutionFailure:
-        pass
+            log.debug("dearmoring %s to %s", tmp, signed_by)
+            req_cmd = require.append({"file": tmp}) if require else [{"file": tmp}]
+            dearmor_ret = __states__['cmd.run'](name="/usr/bin/gpg --dearmor -o '%s' '%s'" % (signed_by, tmp),
+                                                creates=signed_by,
+                                                require=req_cmd)
+            log.debug(dearmor_ret)
+            _propagate_changes(ret, dearmor_ret)
+            salt.utils.files.remove(tmp)
+    else:
+        if not __salt__["file.file_exists"](signed_by):
+            log.debug("storing dearmored key %s in %s", signing_key_url, signed_by)
+            keyfile_ret = __states__['file.managed'](name=signed_by,
+                                                     url=signing_key_url,
+                                                     skip_verify=skip_verify)
+            log.debug(keyfile_ret)
+            _propagate_changes(ret, keyfile_ret)
+    
+    if ret["changes"]:
+        __salt__['aptpkg.refresh_db']()
 
     return ret
