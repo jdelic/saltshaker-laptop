4376ad6cf943f8503524ea740f55a974f6bfcd9b
Jonas Maurus <jonas-github@maurus.net>
2024-04-02 23:43:33 +0200

    simplified script structure

--- srv/salt/basics/desktop/gnome_vertical_workspace_setup.py
+++ srv/salt/basics/desktop/gnome_vertical_workspace_setup.py
@@ -2,26 +2,25 @@
 from gi.repository import Gio
 
 
-def set_keybinding(schema_id, key, value):
-    settings = Gio.Settings.new(schema_id)
-    settings.set_strv(key, value)
-    settings.apply()
-    print(f"Set {key} to {value} in {schema_id}")
-
-
 def main():
+    settings = Gio.Settings.new('org.gnome.desktop.wm.keybindings')
+
+    def set_keybinding(key, value):
+        settings.set_strv(key, value)
+        print(f"Set {key} to {value}")
+
     # Switch workspace
-    set_keybinding('org.gnome.desktop.wm.keybindings', 'switch-to-workspace-up', ['<Super>Page_Up', '<Control><Alt>Up'])
-    set_keybinding('org.gnome.desktop.wm.keybindings', 'switch-to-workspace-down', ['<Super>Page_Down', '<Control><Alt>Down'])
-    set_keybinding('org.gnome.desktop.wm.keybindings', 'switch-to-workspace-left', ['<Super><Alt>Left', '<Control><Alt>Left'])
-    set_keybinding('org.gnome.desktop.wm.keybindings', 'switch-to-workspace-right', ['<Super><Alt>Right', '<Control><Alt>Right'])
+    set_keybinding('switch-to-workspace-up', ['<Super>Page_Up', '<Control><Alt>Up'])
+    set_keybinding('switch-to-workspace-down', ['<Super>Page_Down', '<Control><Alt>Down'])
+    set_keybinding('switch-to-workspace-left', ['<Super><Alt>Left', '<Control><Alt>Left'])
+    set_keybinding('switch-to-workspace-right', ['<Super><Alt>Right', '<Control><Alt>Right'])
 
     # Carry window over to workspace
-    set_keybinding('org.gnome.desktop.wm.keybindings', 'move-to-workspace-up', ['<Super><Shift>Page_Up', '<Control><Shift><Alt>Up'])
-    set_keybinding('org.gnome.desktop.wm.keybindings', 'move-to-workspace-down', ['<Super><Shift>Page_Down', '<Control><Shift><Alt>Down'])
-    set_keybinding('org.gnome.desktop.wm.keybindings', 'move-to-workspace-left', ['<Super><Shift><Alt>Left', '<Control><Shift><Alt>Left'])
-    set_keybinding('org.gnome.desktop.wm.keybindings', 'move-to-workspace-right', ['<Super><Shift><Alt>Right', '<Control><Shift><Alt>Right'])
-
+    set_keybinding('move-to-workspace-up', ['<Super><Shift>Page_Up', '<Control><Shift><Alt>Up'])
+    set_keybinding('move-to-workspace-down', ['<Super><Shift>Page_Down', '<Control><Shift><Alt>Down'])
+    set_keybinding('move-to-workspace-left', ['<Super><Shift><Alt>Left', '<Control><Shift><Alt>Left'])
+    set_keybinding('move-to-workspace-right', ['<Super><Shift><Alt>Right', '<Control><Shift><Alt>Right'])
+    settings.apply()
 
 
 if __name__ == "__main__":
